61a06a64d0dca4366a9d1f592cd985c42334a7d3
jake whiteley <odegramusic@gmail.com>
2018-04-29 17:10:11 +0100

    further cleanup of functions.php and make placeholder images default

--- src/Modules/Images.php
+++ src/Modules/Images.php
@@ -4,6 +4,7 @@ namespace Snap\Core\Modules;
 
 use Snap\Core\Snap;
 use Snap\Core\Hookable;
+use Snap\Core\Utils;
 
 /**
  * Controls custom image sizes and thumbnail support.
@@ -29,19 +30,70 @@ class Images extends Hookable
     public static $size_dropdown_names = [];
 
     /**
+     * The file extensions to check when finding palceholders.
+     *
+     * @since  1.0.0
+     * 
+     * @var array
+     */
+    protected $placeholder_extensions = [];
+
+    /**
+     * The placeholder directory path.
+     *
+     * @since  1.0.0
+     * 
+     * @var array
+     */
+    protected $placeholder_directory = '';
+
+    /**
+     * The placeholder directory path URI.
+     *
+     * @since  1.0.0
+     * 
+     * @var array
+     */
+    protected $placeholder_directory_uri = '';
+
+    /**
+     * The filters to run when booted.
+     *
+     * @since  1.0.0
+     * 
+     * @var array
+     */
+    protected $filters = [
+        'post_thumbnail_html' => 'placeholder_image_fallback',
+        'wp_editor_set_quality' => 'get_upload_quality'
+    ];
+
+    /**
      * Register class conditional filters.
      *
      * @since  1.0.0
      */
     public function boot()
     {
-        // Override the default image compression quality.
-        if (is_numeric(Snap::config('images.default_image_quality'))) {
-            $this->add_filter('wp_editor_set_quality', 'get_upload_quality');
-        }
+        /**
+         * The file extensions to search for when looking for placeholder fallback images.
+         *
+         * @since  1.0.0
+         *
+         * @param  array $extensions The file extension list, in order of search preference.
+         * @return array $extensions The modified file extension list.
+         */
+        $this->placeholder_extensions = apply_filters('snap_placeholder_img_extensions', [ '.jpg', '.svg', '.png' ]);
+
+        $this->placeholder_directory = trailingslashit(get_stylesheet_directory()) . trailingslashit(Snap::config('images.placeholder_dir'));
+
+        $this->placeholder_directory_uri = trailingslashit(get_stylesheet_directory_uri()) . trailingslashit(Snap::config('images.placeholder_dir'));
 
         // Enable post-thumbnail support.
         $this->enable_thumbnail_support();
+
+        // Register all image sizes.
+        $this->register_image_sizes();
 
         // Remove all default image sizes.
         if (Snap::config('images.reset_image_sizes') !== false) {
@@ -49,16 +101,10 @@ class Images extends Hookable
             $this->add_filter('intermediate_image_sizes', 'remove_default_image_sizes');
         }
 
-        // Add custom image sizes if defined.
-        if (! empty(Snap::config('images.image_sizes'))) {
-            // Register all image sizes.
-            $this->register_image_sizes();
-
-            if (! empty(self::$size_dropdown_names)) {
-                $this->add_filter('image_size_names_choose', 'enable_custom_image_sizes');
-            }
+        if (! empty(self::$size_dropdown_names)) {
+            $this->add_filter('image_size_names_choose', 'enable_custom_image_sizes');
         }
-
+    
         // Set default image size dropdown value.
         if (! empty(Snap::config('images.insert_image_default_size'))) {
             $this->add_filter('after_setup_theme', 'set_insert_image_default_size');
@@ -95,9 +141,12 @@ class Images extends Hookable
      *
      * @return  int A number between 0-100.
      */
-    public function get_upload_quality()
+    public function get_upload_quality($quality)
     {
-        return (int) Snap::config('images.default_image_quality');
+        if (is_numeric(Snap::config('images.default_image_quality'))) {
+            return (int) Snap::config('images.default_image_quality');
+        }
+        return $quality;
     }
 
     /**
@@ -125,6 +174,140 @@ class Images extends Hookable
     {
         update_option('image_default_align', 'center');
         update_option('image_default_size', Snap::config('images.insert_image_default_size'));
+    }
+
+
+    /**
+     * If no post_thumbnail was found, find the corresponding placeholder image and return the image HTML.
+     *
+     * @since 1.0.0
+     *
+     * @param  string       $html              The post thumbnail HTML.
+     * @param  int          $post_id           The post ID.
+     * @param  string       $post_thumbnail_id The post thumbnail ID.
+     * @param  string|array $size              The post thumbnail size. Image size or array of width and height
+     *                                         values (in that order). Default 'post-thumbnail'.
+     * @param  string       $attr              Query string of attributes.
+     * @return string                          The image HTML
+     */
+    public function placeholder_image_fallback($html, $post_id, $post_thumbnail_id, $size, $attr)
+    {
+        if ($html === '' && Snap::config('images.placeholder_dir') !== false) {
+            $html = $this->get_placeholder_image($post_id, $post_thumbnail_id, $size, $attr);
+        }
+
+        return $html;
+    }
+
+    /**
+     * Searches for a suitable placeholder fallback image.
+     *
+     * First checks placeholder-${image_size}, then placeholder-${post_type}, then finally placeholder.
+     *
+     * Runs through $this->placeholder_extensions in order when searching for placeholders.
+     *
+     * @since 1.0.0
+     *
+     * @param  string       $html              The post thumbnail HTML.
+     * @param  int          $post_id           The post ID.
+     * @param  string       $post_thumbnail_id The post thumbnail ID.
+     * @param  string|array $size              The post thumbnail size. Image size or array of width and height
+     *                                         values (in that order). Default 'post-thumbnail'.
+     * @param  string       $attr              Query string of attributes.
+     * @return string                          The image HTML
+     */
+    public function get_placeholder_image($post_id, $post_thumbnail_id, $size, $attr = [])
+    {
+        $placeholder_url = false;
+        $original_size = $size;
+
+        if (Utils::get_image_size($size) === false) {
+            $size = 'full';
+        }
+
+        // Search for a size specific placeholder first
+        $placeholder_url = $this->search_for_placeholder('placeholder-' . $size);
+
+        if ($placeholder_url === false) {
+            // Then the post type placeholder
+            $placeholder_url = $this->search_for_placeholder('placeholder-' . get_post_type($post_id));
+        }
+
+        if ($placeholder_url === false) {
+            // Finally a generic placeholder
+            $placeholder_url = $this->search_for_placeholder('placeholder');
+        }
+
+        if ($placeholder_url !== false) {
+            $html = sprintf(
+                '<img src="%s" alt="%s" width="%d" height="%d" %s>',
+                $placeholder_url,
+                get_the_title($post_id),
+                is_array($original_size) ? $original_size[0] : Utils::get_image_width($size),
+                is_array($original_size) ? $original_size[1] : Utils::get_image_height($size),
+                $this->parse_attributes($attr)
+            );
+
+            /**
+             * Filter the placeholder image HTML.
+             *
+             * @since  1.0.0
+             *
+             * @param string $output The HTML output for the placeholder image tag.
+             * @return string $output The HTML output for the placeholder image tag.
+             */
+            return apply_filters('snap_placeholder_img_html', $html);
+        }
+
+        return '';
+    }
+
+    /**
+     * Scans the file system to see if a given file exists with an extension from $this->placeholder_extensions.
+     *
+     * @since  1.0.0
+     * 
+     * @param  string $file_name The placeholder to look for, minus extension.
+     * @return string|bool       false if not found, otherwise the public URI to the found placeholder.
+     */
+    private function search_for_placeholder($file_name)
+    {
+        $placeholder_url = false;
+
+        foreach ($this->placeholder_extensions as $ext) {
+            // check if the file exists
+            $file_path = $this->placeholder_directory . $file_name . $ext;
+            
+            if (file_exists($file_path) === true) {
+                $placeholder_url = $this->placeholder_directory_uri . $file_name . $ext;
+                break;
+            }
+        }
+
+        return $placeholder_url;
+    }
+
+    /**
+     * Parses image $attr array, turning them into HTML.
+     *
+     * @since  1.0.0
+     * 
+     * @param  array $attr  The $attr array.
+     * @return string
+     */
+    private function parse_attributes($attr)
+    {
+        $html = '';
+
+        if (! empty($attr)) {
+            $html = '';
+
+            foreach ($attr as $key => $value) {
+                $html .= sprintf('%s="%s" ', $key, esc_attr($value));
+            }
+        }
+
+        return trim($html);
     }
 
     /**
@@ -156,6 +339,10 @@ class Images extends Hookable
      */
     private function register_image_sizes()
     {
+        if (! empty(Snap::config('images.image_sizes'))) {
+            return;
+        }
+
         // Loop through sizes.
         foreach (Snap::config('images.image_sizes') as $name => $size_info) {
             // Get size properties with basic fallbacks.
--- src/Modules/functions.php
+++ src/Modules/functions.php
@@ -3,6 +3,7 @@
 use Snap\Core\Snap;
 use Snap\Core\Hookable;
 use Snap\Core\Utils;
+use Snap\Core\Router;
 
 /**
  * A helper function for calling Snap::module.
@@ -18,29 +19,7 @@ function snap_render_module($name, $slug = '', $data = null, $extract = false)
     Snap::module($name, $slug, $data, $extract);
 }
 
-/**
- * Counts the number of widgets for a given sidebar ID.
- *
- * @since 1.0.0
- *
- * @see Snap\Core\Utils::get_widget_count
- */
-function snap_get_widget_count($sidebar_id)
-{
-    return Utils::get_widget_count($sidebar_id);
-}
 
-/**
- * Returns the translated role of the current user or for a given user object.
- *
- * @since 1.0.0
- *
- * @see Snap\Core\Utils::get_user_role
- */
-function snap_get_user_role($user = null)
-{
-    return Utils::get_user_role($user);
-}
 
 
 /**
@@ -72,17 +51,7 @@ function snap_debug_hook($hook)
     return Hookable::debug_hook($hook);
 }
 
-/**
- * Gets the current full URL of the page with querystrings, host, and scheme.
- *
- * @since  1.0.0
- *
- * @see Snap\Core\Utils::get_current_url
- */
-function snap_get_current_url($remove_query = false)
-{
-    return Utils::get_current_url($remove_query);
-}
+
 
 
 if (! function_exists('config')) {
@@ -100,6 +69,148 @@ if (! function_exists('config')) {
         return Snap::services()->get('Snap\Core\Config')->get($key, $default);
     }
 }
+
+
+
+
+/*
+ * *************************************************************************
+ * Utility functions
+ *
+ * Some useful functions to make everyday WordPress life easier.
+ * *************************************************************************
+ */
+
+if (! function_exists('snap_get_current_url')) {
+    /**
+     * Gets the current full URL of the page with or without query strings.
+     *
+     * @since  1.0.0
+     *
+     * @see Snap\Core\Utils::get_current_url
+     */
+    function snap_get_current_url($remove_query = false)
+    {
+        return Utils::get_current_url($remove_query);
+    }
+}
+
+
+if (! function_exists('snap_get_current_url_segments')) {
+    /**
+     * Gets the path segments of the curernt URL.
+     *
+     * @since  1.0.0
+     *
+     * @see Snap\Core\Router::get_path_segments
+     */
+    function snap_get_current_url_segments()
+    {
+        return Snap::services()->get(Router::class)->get_path_segments();
+    }
+}
+
+if (! function_exists('snap_get_widget_count')) {
+    /**
+     * Counts the number of widgets for a given sidebar ID.
+     *
+     * @since 1.0.0
+     *
+     * @see Snap\Core\Utils::get_widget_count
+     */
+    function snap_get_widget_count($sidebar_id)
+    {
+        return Utils::get_widget_count($sidebar_id);
+    }
+}
+
+if (! function_exists('snap_get_user_role')) {
+    /**
+     * Returns the translated role of the current user or for a given user object.
+     *
+     * @since 1.0.0
+     *
+     * @see Snap\Core\Utils::get_user_role
+     */
+    function snap_get_user_role($user = null)
+    {
+        return Utils::get_user_role($user);
+    }
+}
+
+
+
+/*
+ * *************************************************************************
+ * Image helpers
+ *
+ * A collection of functions for getting meta information about image sizes.
+ * *************************************************************************
+ */
+
+if (! function_exists('snap_get_image_sizes')) {
+    /**
+     * Get size information for all currently registered image sizes.
+     *
+     * @since  1.0.0
+     *
+     * @see Snap\Core\Utils::get_image_sizes
+     */
+    function snap_get_image_sizes()
+    {
+        return Utils::get_image_sizes();
+    }
+}
+
+if (! function_exists('snap_get_image_size')) {
+    /**
+     * Get size information for a specific image size.
+     *
+     * @since  1.0.0
+     *
+     * @see Snap\Core\Utils::get_image_size
+     */
+    function snap_get_image_size($size)
+    {
+        return Utils::get_image_size($size);
+    }
+}
+
+if (! function_exists('snap_get_image_width')) {
+    /**
+     * Get the px width of a specific image size.
+     *
+     * Really useful for HTML img tags and schema.
+     *
+     * @since  1.0.0
+     *
+     * @see Snap\Core\Utils::get_image_width
+     */
+    function snap_get_image_width($size)
+    {
+        return Utils::get_image_width($size);
+    }
+}
+
+if (! function_exists('snap_get_image_height')) {
+    /**
+     * Get the px height of a specific image size.
+     *
+     * Really useful for HTML img tags and schema.
+     *
+     * @since  1.0.0
+     *
+     * @see Snap\Core\Utils::get_image_height
+     */
+    function snap_get_image_height($size)
+    {
+        return Utils::get_image_height($size);
+    }
+}
+
+
+
+
 
 
 
@@ -129,260 +240,7 @@ function snap_render_view($slug, $name = '')
 }
 
 
-/**
- * Get size information for all currently registered image sizes.
- *
- * @global $_wp_additional_image_sizes
- *
- * @since  1.0.0
- *
- * @return array Data for all currently registered image sizes.
- */
-function snap_get_image_sizes()
-{
-    global $_wp_additional_image_sizes;
 
-    $sizes = [];
-
-    foreach (get_intermediate_image_sizes() as $size) {
-        if (in_array($size, ['thumbnail', 'medium', 'medium_large', 'large'])) {
-            $sizes[ $size ] = [
-                'width' => get_option("{$size}_size_w"),
-                'height' => get_option("{$size}_size_h"),
-                'crop' => (bool) get_option("{$size}_crop"),
-            ];
-        } elseif (isset($_wp_additional_image_sizes[ $size ])) {
-            $sizes[ $size ] = [
-                'width'  => $_wp_additional_image_sizes[ $size ]['width'],
-                'height' => $_wp_additional_image_sizes[ $size ]['height'],
-                'crop'   => $_wp_additional_image_sizes[ $size ]['crop'],
-            ];
-        }
-    }
-
-    return $sizes;
-}
-
-
-/**
- * Get size information for a specific image size.
- *
- * @since  1.0.0
- *
- * @param  string $size The image size for which to retrieve data.
- * @return bool|array Size data about an image size or false if the size doesn't exist.
- */
-function snap_get_image_size($size)
-{
-    $sizes = snap_get_image_sizes();
-
-    if (is_string($size) && isset($sizes[ $size ])) {
-        return $sizes[ $size ];
-    }
-
-    return false;
-}
-
-
-/**
- * Get the width of a specific image size.
- *
- * Really useful for HTML img tags.
- *
- * @since  1.0.0
- *
- * @param  string $size The image size for which to retrieve data.
- * @return bool|int Width of an image size or false if the size doesn't exist.
- */
-function snap_get_image_width($size)
-{
-    // Get the size meta array.
-    $size = snap_get_image_size($size);
-
-    if ($size !== false) {
-        return false;
-    }
-
-    if (isset($size['width'])) {
-        return (int) $size['width'];
-    }
-
-    return false;
-}
-
-
-/**
- * Get the height of a specific image size.
- *
- * Really useful for HTML img tags.
- *
- * @since  1.0.0
- *
- * @param  string $size The image size for which to retrieve data.
- * @return bool|int Height of an image size or false if the size doesn't exist.
- */
-function snap_get_image_height($size)
-{
-    // Get the size meta array.
-    $size = snap_get_image_size($size);
-
-    if ($size !== false) {
-        return false;
-    }
-
-    if (isset($size['height'])) {
-        return (int) $size['height'];
-    }
-
-    return false;
-}
-
-
-/**
- * Renders a post thumbnail to the page.
- *
- * If the post has no featured image, falls back to placeholder-{size}, then placeholder-{post_type}, then to placeholder.
- * If a placeholder is used, the width and height will be set according to the registered dimensions for the given $size.
- *
- * @since  1.0.0
- *
- * @param  string $size The registered size of image to find
- * @param  array  $args Array of attrs => values to apply to the image
- */
-function snap_the_post_thumbnail($size = 'full', $args = [])
-{
-    echo snap_get_the_post_thumbnail(get_the_id(), $size, $args);
-}
-
-
-/**
- * Returns the post thumbnail img element.
- *
- * If the post has no featured image, falls back to placeholder-{size}, then placeholder-{post_type}, then to placeholder.
- * If a placeholder is used, the width and height will be set according to the registered dimensions for the given $size.
- *
- * @since  1.0.0
- *
- * @param  int|object $post  The post ID or object to find the image for
- * @param  string     $size  The registered size of image to find
- * @param  array      $args  Array of attrs => values to apply to the image
- * @return string The image HTML
- */
-function snap_get_the_post_thumbnail($post, $size = 'full', $args = [])
-{
-    // If the size provided doesn't exist, default to full.
-    if (is_string($size) && snap_get_image_size($size) === false) {
-        $size = 'full';
-    }
-
-    // Check we have a post thumbnail.
-    if (has_post_thumbnail($post)) {
-        return get_the_post_thumbnail($post, $size, $args);
-    }
-
-    $placeholder_src = snap_get_the_post_thumbnail_url($post, $size);
-
-    if ($placeholder_src === false) {
-        return false;
-    }
-
-    $output = sprintf(
-        '<img src="%s" alt="%s" width="%s" height="%s" %s>',
-        $placeholder_src,
-        get_the_title($post),
-        snap_get_image_width($size),
-        snap_get_image_height($size),
-        ( isset($args['class']) ? 'class="' . $args['class'] . '"' : '' )
-    );
-
-    /**
-     * Filter the placeholder image HTML.
-     *
-     * @since  1.0.0
-     *
-     * @param string $output The HTML output for the placeholder image tag.
-     * @return string $output The HTML output for the placeholder image tag.
-     */
-    return apply_filters('snap_placeholder_img_html', $output);
-}
-
-
-/**
- * Returns the post thumbnail URL.
- *
- * If the post has no featured image, falls back to placeholder-{size}, then placeholder-{post_type}, then to placeholder.
- * If a placeholder is used, the width and height will be set according to the registered dimensions for the given $size.
- *
- * @since  1.0.0
- *
- * @param  int|object $post  The post ID or object to find the image for.
- * @param  string     $size  The registered size of image to find.
- * @return string|bool The image URL or false if none found.
- */
-function snap_get_the_post_thumbnail_url($post, $size = 'full')
-{
-    // If the size provided doesn't exist, default to full.
-    if (snap_get_image_size($size) === false) {
-        $size = 'full';
-    }
-
-    // Check we have a post thumbnail.
-    if (has_post_thumbnail($post)) {
-        return get_the_post_thumbnail_url($post, $size);
-    }
-
-    /**
-     * The file extensions to search for when looking for placeholder fallback images.
-     *
-     * @since  1.0.0
-     *
-     * @param array $extensions The file extension list, in order of search preference.
-     * @return array $extensions The modified file extension list.
-     */
-    $extensions = apply_filters('snap_placeholder_img_extensions', [ '.jpg', '.svg', '.png' ]);
-
-    // Get the theme defined placeholder image directory path.
-    $placeholder_directory = trailingslashit(Snap::config('imgages.placeholder_dir'));
-
-    // Get relative path to placeholder folder.
-    $base = $placeholder_directory . 'placeholder-' . $size;
-
-    // look for each extension.
-    foreach ($extensions as $ext) {
-        $file_path = trailingslashit(get_stylesheet_directory()) . $base . $ext;
-
-        if (file_exists($file_path) === true) {
-            return trailingslashit(get_stylesheet_directory_uri()) . $base . $ext;
-        }
-    }
-
-    // If no size img found, search for generic.
-    $base = $placeholder_directory . 'placeholder-' . get_post_type($post);
-
-    foreach ($extensions as $ext) {
-        $file_path = trailingslashit(get_stylesheet_directory()) . $base . $ext;
-
-        if (file_exists($file_path) === true) {
-            return trailingslashit(get_stylesheet_directory_uri()) . $base . $ext;
-        }
-
-        $path = false;
-    }
-    
-    // If no specific img found, search for the generic placeholder.
-    $base = $placeholder_directory . 'placeholder';
-
-    foreach ($extensions as $ext) {
-        // check if the file exists
-        $file_path = trailingslashit(get_stylesheet_directory()) . $base . $ext;
-        
-        if (file_exists($file_path) === true) {
-            return trailingslashit(get_stylesheet_directory_uri()) . $base . $ext;
-        }
-    }
-
-    return false;
-}
 
 
 
