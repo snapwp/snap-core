7ee8a0e8ea3329aaa5ba522649d7989c3ec0b2fa
Jake Whiteley <jake@craftedbygc.com>
2018-10-06 14:16:18 +0100

    Ensure published files are tagged and publish correctly

--- src/Services/Provider.php
+++ src/Services/Provider.php
@@ -3,6 +3,8 @@
 namespace Snap\Services;
 
 use Snap\Core\Snap;
+use RecursiveIteratorIterator;
+use RecursiveArrayIterator;
 
 /**
  * Base Provider class.
@@ -15,39 +17,49 @@ class Provider implements Interfaces\Provider
      * @since 1.0.0
      * @var array
      */
-    protected static $publishes = [
-        'directories' => [],
-    ];
+    protected static $publishes = [];
 
     /**
      * Called after all service providers have been registered and are available.
      *
      * @since 1.0.0
      */
-    public function boot() {}
+    public function boot()
+    {
+    }
 
     /**
      * Register any services into the container.
      *
      * @since 1.0.0
      */
-    public function register() {} 
+    public function register()
+    {
+    }
 
     /**
      * Returns the package's files to publish.
      *
      * @since  1.0.0
-     * 
+     *
      * @param  string $type Default null. The subset of files to return.
      * @return array
      */
     public static function get_files_to_publish($type = null)
     {
-        if ($type !== null) {
-            return static::$publishes[$type];
+        if (!isset(static::$publishes[ static::class ])) {
+            return [];
         }
-        
-        return static::$publishes;
+
+        if ($type !== null) {
+            return static::$publishes[ static::class ][ $type ];
+        }
+
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveArrayIterator(static::$publishes[ static::class ])
+        );
+
+        return \iterator_to_array($iterator);
     }
 
     /**
@@ -72,9 +84,9 @@ class Provider implements Interfaces\Provider
      * @param  string $source Full path to the source directory.
      * @param  string $target Path to copy the source to - relative to the theme root.
      */
-    protected function publishes_directory($source, $target)
+    protected function publishes_directory($source, $target, $tag = 'directories')
     {
-        static::$publishes['directories'][$source] = $target;
+        static::$publishes[ static::class ][ $tag ][ $source ] = $target;
     }
 
     /**
@@ -86,6 +98,6 @@ class Provider implements Interfaces\Provider
      */
     protected function publishes_config($source)
     {
-        static::$publishes['directories'][$source] = '/config';
+        static::$publishes[ static::class ]['config'][ $source ] = '/config';
     }
 }
