a9fb11fd5558492dd9429583eb10a07858a3b588
Jake Whiteley <jake@craftedbygc.com>
2018-10-11 19:39:27 +0100

    Add docs

--- src/Templating/Templating_Interface.php
+++ src/Templating/Templating_Interface.php
@@ -2,11 +2,33 @@
 
 namespace Snap\Templating;
 
+/**
+ * Interface to ensure interopability between templating engines.
+ */
 interface Templating_Interface
 {
+	/**
+     * Renders a view template.
+     * 
+     * @param  string $slug The current slug to render relative to the views directory.
+     * @param  array  $data Data to pass to this template.
+     * @return void
+     */
     public function render($slug, $data = []);
     
+    /**
+     * Renders a partial template.
+     * 
+     * @param  string $slug The current slug to render relative to the partials directory.
+     * @param  array  $data Data to pass to this template.
+     * @return void
+     */
     public function partial($slug, $data = []);
 
+    /**
+     * Should return the parent view for the current request.
+     * 
+     * @return string
+     */
     public function get_current_view();
 }
