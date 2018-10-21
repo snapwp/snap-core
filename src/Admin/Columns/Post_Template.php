f53f69cc20aa3c0498d57991b0ed3264f0f761f8
Jake Whiteley <jake@craftedbygc.com>
2018-10-21 22:26:29 +0100

    Delete modules/admin

--- src/Admin/Columns/Post_Template.php
+++ src/Admin/Columns/Post_Template.php
@@ -1,16 +1,13 @@
 <?php
 
-namespace Snap\Modules;
+namespace Snap\Admin\Columns;
 
 use Snap\Core\Hookable;
-use Snap\Core\Snap;
 
 /**
- * Additional code directly affecting admin area.
- *
- * @since  1.0.0
+ * Add post template column to post types.
  */
-class Admin extends Hookable
+class Post_template extends Hookable
 {
     /**
      * No need to run this Hookable when on a public request.
@@ -27,8 +24,6 @@ class Admin extends Hookable
      * @var array
      */
     protected $filters = [
-        // Add additional mime types to the media filter dropdown.
-        'post_mime_types' => 'additional_mime_types',
 
         // Add custom columns.
         'manage_pages_columns' => 'register_columns',
@@ -45,64 +40,9 @@ class Admin extends Hookable
      * @var array
      */
     protected $actions = [
-        // Flush rewrite rules after theme activation - always a good idea!
-        'after_switch_theme' => 'flush_rewrite_rules',
-
-        // Output custom column content.
         'manage_pages_custom_column' => 'populate_columns',
         'manage_posts_custom_column' => 'populate_columns',
     ];
-    
-    /**
-     * Add some additional mime type filters to media pages.
-     *
-     * @since  1.0.0
-     *
-     * @param  array $post_mime_types The current list of mime types.
-     * @return array The original list with our additional types.
-     */
-    public function additional_mime_types($post_mime_types)
-    {
-        $additional_mime_types = [
-            'application/msword' => [
-                \__('Word Docs', 'snap'),
-                \__('Manage Word Docs', 'snap'),
-                \_n_noop('Word Doc <span class="count">(%s)</span>', 'Word Docs <span class="count">(%s)</span>'),
-            ],
-            'application/vnd.ms-excel' => [
-                \__('Excel Docs', 'snap'),
-                \__('Manage Excel Docs', 'snap'),
-                \_n_noop('Excel Doc <span class="count">(%s)</span>', 'Excel Docs <span class="count">(%s)</span>'),
-            ],
-            'application/pdf' => [
-                \__('PDFs', 'snap'),
-                \__('Manage PDFs', 'snap'),
-                \_n_noop('PDF <span class="count">(%s)</span>', 'PDFs <span class="count">(%s)</span>'),
-            ],
-            'application/zip' => [
-                \__('ZIPs', 'snap'),
-                \__('Manage ZIPs', 'snap'),
-                \_n_noop('ZIP <span class="count">(%s)</span>', 'ZIPs <span class="count">(%s)</span>'),
-            ],
-            'text/csv' => [
-                \__('CSVs', 'snap'),
-                \__('Manage CSVs', 'snap'),
-                \_n_noop('CSV <span class="count">(%s)</span>', 'CSVs <span class="count">(%s)</span>'),
-            ],
-        ];
-
-        return \array_merge($post_mime_types, $additional_mime_types);
-    }
-
-    /**
-     * Flush rewrite rules for custom post types after theme is switched.
-     *
-     * @since  1.0.0
-     */
-    public function flush_rewrite_rules()
-    {
-        \flush_rewrite_rules();
-    }
 
     /**
      * Add new columns to admin views.
