4c420c98f7af4841d0a732cd32c3a7a73c9f7b9a
jake whiteley <odegramusic@gmail.com>
2024-03-30 11:16:40 +0000

    improve disable lazy loading

--- src/Admin/DisableLazyLoading.php
+++ src/Admin/DisableLazyLoading.php
@@ -12,16 +12,26 @@ class DisableLazyLoading extends Hookable
     public function boot(): void
     {
         $this->addFilter('wp_lazy_loading_enabled', '__return_false');
-        $this->addFilter('post_thumbnail_html', 'stripLazyAttr');
+        $this->addFilter('post_thumbnail_html', 'stripLazyAttribute');
+
+        $this->addFilter('wp_img_tag_add_decoding_attr', '__return_false');
+        $this->addFilter('wp_get_attachment_image_attributes', 'removeDecodingAttribute');
     }
 
     /**
-     * Replaces links found in the content with the link markup we want.
-     *
-     * A bit heavy-handed, but should be fine.
+     * Remove loading-lazy from post thumbnails.
      */
-    public function stripLazyAttr(string $content): string
+    public function stripLazyAttribute(string $content): string
     {
         return str_replace('loading="lazy"', '', $content);
+    }
+
+    /**
+     * Remove decoding attribute from images.
+     */
+    public function removeDecodingAttribute(array $attributes): array
+    {
+        unset($attributes['decoding']);
+        return $attributes;
     }
 }
